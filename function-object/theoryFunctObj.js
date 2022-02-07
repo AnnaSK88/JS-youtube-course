9131cdfa9edbc5801fcf16ed8f1a11851c5383d3
Anna SK <anna.sapun89@gmail.com>
2022-02-07 12:45:38 +0100

    fix files

--- function-object/theoryFunctObj.js
+++ function-object/theoryFunctObj.js
@@ -14,3 +14,20 @@ let picture = {
 
 // массив, функция, число* и строки* - это обьекты
 // * Число и Строки только ведут как обькты, а так примиты
+
+
+
+
+console.log("Hello");// выражение
+console.dir({Anna: 32, Kiryl: 33, Lera: 8})
+console.table({view: 'forest', picture: "Animal"})
+console
+
+//console - obj
+//log - method
+//. - точечная запись
+// () - вызов метода
+// "Hello" - argument/ type string
+
+//dir - отображает все свойства обьекта
+//table - все св-ва объектв в табличном виде
