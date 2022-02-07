3eb8c49e597c9663db641c84346e8bc2e3064c03
Anna SK <anna.sapun89@gmail.com>
2022-02-07 12:30:29 +0100

    add explanation func and obj

---
+++ function-object/function.js
@@ -0,0 +1,13 @@
+console.log("Hello");
+console.dir({Anna: 32, Kiryl: 33, Lera: 8})
+console.table({view: 'forest', picture: "Animal"})
+
+
+//console - obj
+//log - method
+//. - точечная запись
+// () - вызов метода
+// "Hello" - argument/ type string
+
+//dir - отображает все свойства обьекта
+//table - все св-ва объектв в табличном виде
