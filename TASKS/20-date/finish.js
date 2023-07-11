23a46ab260f3c0d358e16207a84d0ec6d395aa20
Anna SK <anna.sapun89@gmail.com>
2023-07-11 12:11:30 +0200

    add solutions for 14-20 tasks

---
+++ TASKS/20-date/finish.js
@@ -0,0 +1,10 @@
+/** ЗАДАЧА 20 - Дата
+ *
+ * 1. Создайте переменную и присвойте ей текущее время в миллисекундах
+ *
+ * 2. Выведите значение в консоль
+ */
+const timeInMyCity = new Date()
+const currentDateInMs = timeInMyCity.getTime()
+
+console.log(currentDateInMs)
